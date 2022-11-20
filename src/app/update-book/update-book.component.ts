dbef408d1a97cc88fa00d9fcfa6e2be218aaca98
Dipankar Dutta <dipankar8dutta@gmail.com>
2022-11-20 22:11:03 +0530

    COMMIT-6

--- src/app/update-book/update-book.component.ts
+++ src/app/update-book/update-book.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookServiceService } from '../book-service.service';
+import { Book } from '../book/book';
+import { BookComponent } from '../book/book.component';
 
 @Component({
   selector: 'app-update-book',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateBookComponent implements OnInit {
 
-  constructor() { }
+  book:Book=new Book("","","","",0);
+  id:number=0;
+  sub:any="";
+  constructor(private route:ActivatedRoute, private bService:BookServiceService) { }
 
   ngOnInit(): void {
+    this.sub=this.route.params.subscribe(params => {
+      this.id = params['id'];
+      });
+      
+      let resp=this.bService.getBook(this.id);
+      resp.subscribe(((data)=>this.book=<Book>data));
+  }
+
+  public updateBook(){
+    let resp=this.bService.updateBook(this.book);
+    resp.subscribe((data)=>{});
   }
 
 }
